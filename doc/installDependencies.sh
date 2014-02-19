34bb0bb088780fb3696d8fd42f7865b2b2ad575a
Alex Stephens <a.stephens4@gmail.com>
2014-02-19 08:44:37 -0500

    Added umlet UML digramming tool to dependencies

--- doc/installDependencies.sh
+++ doc/installDependencies.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 # install the executables for the build environment
-sudo apt-get install git cmake ia32-libs wget
+sudo apt-get install git cmake ia32-libs wget umlet
 
 # download the 3rdParty libraries
 wget "http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.8/opencv-2.4.8.zip/download" -O ../raspi/3rdParty/opencv-2.4.8.zip
