30a67dc70a065e8cef862253998dd15b18b406c3
Alex Stephens <a.stephens4@gmail.com>
2014-02-26 09:04:38 -0500

    Changed the simple test to open a test image and perform a Canny filter on it, and display the contours. Need to add rpath options in Makefile

--- raspi/distribute.sh
+++ raspi/distribute.sh
@@ -27,13 +27,13 @@ mkdir -p ./$OUTDIR/{lib,bin}
 cp -a ./common/$BUILD_VERSION/lib/* $OUTDIR/lib
 
 # copy the non-third party libraries into $OUTDIR/lib
-cp -a ./dist/$BUILD_VERSION/lib/* $OUTDIR/lib
+cp -a ./$BUILD_VERSION/lib/* $OUTDIR/lib
 
 # copy the 3rdParty binaries into $OUTDIR/bin
 cp -a ./common/$BUILD_VERSION/bin/* $OUTDIR/bin
 
 # copy the non-third party binaries into $OUTDIR/bin
-cp -a ./dist/$BUILD_VERSION/bin/* $OUTDIR/bin
+cp -a ./$BUILD_VERSION/bin/* $OUTDIR/bin
 
 # pack $OUTDIR into a zipped tar
 tar czfp $OUTDIR.tgz ./$OUTDIR && rm -rf $OUTDIR
--- raspi/simple_test/main.cpp
+++ raspi/simple_test/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 int main(void)
 {
-    std::cout << "Testing build with opencv" << std::endl;
-    cv::Matx<float,4,4> sqMat;
-    std::cout << sqMat << std::endl;
+    cv::Mat image = cv::imread("test.jpg");
+    cv::Mat edges;
+    cv::Canny(image, edges, 125, 250);
+    cv::imshow("Look at these contours!\n", edges);
+    while(1) {};
 }
