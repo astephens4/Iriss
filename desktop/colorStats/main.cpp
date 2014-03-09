6aab9fb9ea44969dfc2099daa3d8e8595ece2b54
Alex Stephens <a.stephens4@gmail.com>
2014-03-09 15:06:37 -0400

    Color stats now splits the image into B, G, R, H, S, V, components and displays each separately with the min and max values with a point on them

--- desktop/colorStats/main.cpp
+++ desktop/colorStats/main.cpp
@@ -5,6 +5,59 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+std::string cv_depth_str(int depth)
+{
+    switch(depth) {
+        case CV_8U:
+            return "CV_8U";
+        case CV_8S:
+            return "CV_8S";
+        case CV_16U:
+            return "CV_16U";
+        case CV_16S:
+            return "CV_16S";
+        case CV_32S:
+            return "CV_32S";
+        case CV_32F:
+            return "CV_32F";
+        case CV_64F:
+            return "CV_64F";
+    }
+    return "OOPS!";
+}
+
+std::string cv_type_str(int depth, int chan)
+{
+    std::ostringstream oss;
+    oss << cv_depth_str(depth);
+    oss << "C" << chan;
+    return oss.str();
+}
+
+void show_point(cv::Mat image, cv::Point location, cv::Scalar color, unsigned int size)
+{
+    if(size > 10) {
+        size = 10;
+    }
+
+    int x0 = location.x,
+        y0 = location.y;
+    int x1 = location.x + size,
+        y1 = location.y + size;
+    cv::Size bloop = image.size();
+    if(x1 > bloop.width) {
+        x1 -= 2*size;
+    }
+    if(y1 > bloop.height) {
+        y1 -= 2*size;
+    }
+    x0 = x0 < x1 ? x0 : x1;
+    y0 = y0 < y1 ? y0 : y1;
+
+    cv::Mat ptSection(image, cv::Rect(x0, y0, size, size));
+    ptSection = color;
+}
+
 int main(int nargs, char **argv)
 {
     std::string imageFile;
@@ -22,21 +75,62 @@ int main(int nargs, char **argv)
 
     cv::Mat asHSV;
     cv::cvtColor(image, asHSV, CV_BGR2HSV);
+    std::cerr << "Some info on the image:\n"
+              << "Dimensions: " << image.size() << std::endl
+              << "Type stored: " << cv_type_str(image.depth(), image.channels()) << std::endl;
 
     // min, max, location
     double min, max;
-    cv::Point minLoc(0,0), maxLoc(100,100);
+    cv::Point minLoc;
+    cv::Point maxLoc;
 
-    cv::minMaxLoc(image, &min, &max, &minLoc, &maxLoc);
-    std::cout << "BGR;" << min << "," << max << std::endl;
+    cv::Mat bgrChannels[3];
+    cv::split(image, bgrChannels);
+
+    for(int i = 0; i < image.channels(); ++i) {
+        cv::minMaxLoc(bgrChannels[i], &min, &max, &minLoc, &maxLoc);
+        std::ostringstream oss;
+        switch(i) {
+            case 0:
+                oss << "B;";
+                break;
+            case 1:
+                oss << "G;";
+                break;
+            case 2:
+                oss << "R;";
+                break;
+        }
+        oss << "MIN:" << minLoc << "{" << min << "}" << "," << "MAX:" << maxLoc << "{" << max << "}";
+        show_point(bgrChannels[i], minLoc, cv::Scalar(0), 4);
+        show_point(bgrChannels[i], maxLoc, cv::Scalar(255), 4);
+        imshow(oss.str(), bgrChannels[i]);
+        std::cout << oss.str() << std::endl;
+    }
     
-//    image.at<cv::Scalar>(minLoc) = cv::Scalar(0, 0, 0);
-//    image.at<cv::Scalar>(maxLoc) = cv::Scalar(255, 255, 255);
-//    cv::imshow("Original image", image);
+    cv::Mat hsvChannels[3];
+    cv::split(asHSV, hsvChannels);
 
-    cv::minMaxLoc(asHSV, &min, &max);
-    std::cout << "HSV;" << min << "," << max << std::endl;
-    //std::cout << "HSV;" << min << "," << max << "," << minLoc << "," << maxLoc << std::endl;
+    for(int i = 0; i < image.channels(); ++i) {
+        cv::minMaxLoc(hsvChannels[i], &min, &max, &minLoc, &maxLoc);
+        std::ostringstream oss;
+        switch(i) {
+            case 0:
+                oss << "H;";
+                break;
+            case 1:
+                oss << "S;";
+                break;
+            case 2:
+                oss << "V;";
+                break;
+        }
+        oss << "MIN:" << minLoc << "{" << min << "}" << "," << "MAX:" << maxLoc << "{" << max << "}";
+        show_point(hsvChannels[i], minLoc, cv::Scalar(0), 4);
+        show_point(hsvChannels[i], maxLoc, cv::Scalar(255), 4);
+        imshow(oss.str(), hsvChannels[i]);
+        std::cout << oss.str() << std::endl;
+    }
 
-    
+    cv::waitKey();
 }
