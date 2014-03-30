130242b19da987fe01310690cb43c51711753b0b
Alex Stephens <a.stephens4@gmail.com>
2014-03-29 22:05:05 -0400

    Fixed the opencv downloaw path. again. meh

--- doc/setupDevEnvironment.sh
+++ doc/setupDevEnvironment.sh
@@ -2,7 +2,7 @@
 
 # install the executables for the build environment
 echo "Downloading local dependencies from Ubuntu repositories..."
-sudo apt-get install git cmake ia32-libs wget umlet build-essential
+sudo apt-get install git cmake ia32-libs wget umlet build-essential arduino-core
 
 echo "Getting Raspberry Pi toolchain from the official Github"
 git clone https://github.com/raspberrypi/tools.git ./raspi_tc
@@ -15,4 +15,4 @@ rm -rf ./raspi_tc
 
 # download the 3rdParty libraries
 echo "Downloading 3rdPary Libraries"
-wget "http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.8/opencv-2.4.8.zip/download" -O ../raspi/3rdParty/opencv-2.4.8.zip
+wget "http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.8/opencv-2.4.8.zip/download" -O ../3rdParty/opencv-2.4.8.zip
